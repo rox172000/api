8d942621354a1bde29a2384e1999964eaa099e2d
rox172000 <67087061+rox172000@users.noreply.github.com>
2024-07-28 21:13:12 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+ $cn = new mysqli("localhost", "id22414700_id16698443_root","LVEgj74h-c6fDSlS","id22414700_id16698443_arquitectura");
+?>
